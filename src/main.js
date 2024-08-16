d9be3f23fa3f87698f08c807c8134fd712e6737c
lamlankosi <lamlanomnganga9@gmail.com>
2024-08-16 14:59:07 +0200

    set Up

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'boostrap/dist/css/bootstrap.min.css'
+import 'boostrap'
 
 createApp(App).use(store).use(router).mount('#app')
